166ba4e8aca6da5f9affccf85d040b668d9cfc0b
sooye7 <sooye7@naver.com>
2023-02-10 12:58:12 +0900

    OCR 기능 주석처리

--- src/main/java/com/spring/companion/CompanionServerApplication.java
+++ src/main/java/com/spring/companion/CompanionServerApplication.java
@@ -1,6 +1,6 @@
 package com.spring.companion;
 
-import org.opencv.core.Core;
+//import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
